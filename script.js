f6f6832ad2f5a7b5eea61fbc44f4d1d31c593157
Ch3ssman <jbau2003@gmail.com>
2024-05-03 22:02:19 +1200

    Update styles.css

---
+++ script.js
@@ -0,0 +1,7 @@
+document.querySelector('.btn').addEventListener('click', function() {
+    document.getElementById("pop-up-second-wrapper").style.display = "none";   
+});
+
+// document.querySelector('#main-section').addEventListener('click', function() {
+//     document.getElementById("pop-up-second-wrapper").style.display = "block";   
+// });
